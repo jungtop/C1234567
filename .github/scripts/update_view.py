17fd7044c786d822a93901d8588e2c3df98f9496
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-03-27 12:45:14 +0530

    added token

--- .github/scripts/update_view.py
+++ .github/scripts/update_view.py
@@ -1,9 +1,10 @@
 from automatic_view_updater.update_view import update_view
 import sys
 
-def update(issue_cmnt):
-    update_view(issue_cmnt)
+def update(issue_cmnt,token):
+    update_view(issue_cmnt,token)
 
 if __name__ == "__mian__":
     issue_cmnt = sys.argv[1]
-    update(issue_cmnt)
+    token = sys.argv[2]
+    update(issue_cmnt,token)
